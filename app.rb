d0ec502fd8739edc0e9368c3159163ccb4cbeba0
Jessica Gordon <ketchesj@gmail.com>
2021-11-29 15:50:07 +0000

    trial cat image using html string

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  'bluetooth'
+end
+
+get '/cat' do
+  "<div>
+    <img src='https://imgur.com/jFaSxym' alt='cat'>
+  </div>"
 end
