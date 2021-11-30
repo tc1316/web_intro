cf583ce2381a82da31ef471a3dd5ad82064364d2
Jessica Gordon <ketchesj@gmail.com>
2021-11-30 14:25:02 +0000

    adds cat-form route to post catname to named-cat

--- app.rb
+++ app.rb
@@ -14,8 +14,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @catname = params[:catname]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
